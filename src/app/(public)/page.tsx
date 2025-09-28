e28c4544e4b5d2119959fea81f3f47ddeb32529c
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-09-28 20:14:43 +0600

    next project created and blogs fetch done for home page

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import BlogCard from "@/components/modules/Blogs/BlogCard";
+import Hero from "@/components/modules/Home/Hero";
+
+export default async function HomePage() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/post`,{
+    next: {
+      revalidate: 30,
+    }
+  });
+  const { data: blogs } = await res.json();
+
+  return (
+    <div>
+      <Hero />
+      <h2 className="text-center my-5 text-4xl">Featured Posts</h2>
+      <div className="grid grid-cols-3 gap-4 max-w-6xl mx-auto">
+        {blogs.slice(0, 3).map((blog: any) => (
+          <BlogCard key={blog.id} post={blog} />
+        ))}
+      </div>
+    </div>
+  );
+}
